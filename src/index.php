029ca5745979c1b858731cc2600fa61c1f8a4acc
HEsIDenA <91245353+HEsIDenA@users.noreply.github.com>
2023-02-23 09:35:40 +0100

    Including the navigation

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/welcome-css.css">
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+    integrity="sha256-2KoK7MbOxypgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+    crossorigin="anonymous">
+    </script>
+    <script>
+        $(function (){
+            $("nav").load("html/navigation.html")
+        })
+    </script>
+</head>
+
+<body>
+<div id="nav">
+</div>
+
+</body>
+
+</html>
